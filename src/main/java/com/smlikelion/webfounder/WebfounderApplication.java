f123fd9bed79775e4c10dcbad56ab878ecafe978
summit45 <brandy2145@gmail.com>
2023-11-13 10:56:27 +0900

    [setting] initial settings

---
+++ src/main/java/com/smlikelion/webfounder/WebfounderApplication.java
@@ -0,0 +1,13 @@
+package com.smlikelion.webfounder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfounderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfounderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smlikelion/webfounder/WebfounderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smlikelion.webfounder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfounderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
