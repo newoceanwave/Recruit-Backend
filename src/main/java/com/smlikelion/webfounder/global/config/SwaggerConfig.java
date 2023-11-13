4bf079c2be943b195f60a8e471dc8782cf76a3e2
summit45 <brandy2145@gmail.com>
2023-11-13 13:06:56 +0900

    [setting] swagger 추가

---
+++ src/main/java/com/smlikelion/webfounder/global/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.smlikelion.webfounder.global.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//http://localhost:8080/swagger-ui/index.html
+@Configuration
+public class SwaggerConfig {
+    private final String TITLE = "SOOKMYUNG LIKELION";
+    private final String DESCRIPTION = "숙명여대 멋쟁이사자처럼 리쿠르팅 API";
+    private final String VERSION = "V1.0.0";
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION)
+                );
+    }
+
+}
