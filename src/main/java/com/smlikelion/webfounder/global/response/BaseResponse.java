1e8fb3d2d2310a3b3b50044fe025183826f766ca
summit45 <brandy2145@gmail.com>
2023-11-16 23:57:05 +0900

    [setting] 공통 사용 파일 추가

---
+++ src/main/java/com/smlikelion/webfounder/global/response/BaseResponse.java
@@ -0,0 +1,37 @@
+package com.smlikelion.webfounder.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseResponse<T> {
+    private Boolean isSuccess;
+    private int code;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    //성공
+    public BaseResponse(T result) {
+        this.isSuccess = ErrorCode.SUCCESS.getIsSuccess();
+        this.code = ErrorCode.SUCCESS.getCode();
+        this.message = ErrorCode.SUCCESS.getMessage();
+        this.result = result;
+    }
+
+    // 오류 발생
+    public BaseResponse(ErrorCode errorCode) {
+        this.isSuccess = errorCode.getIsSuccess();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public BaseResponse(ErrorCode errorCode, T result) {
+        this.isSuccess = errorCode.getIsSuccess();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+        this.result = result;
+    }
+}
