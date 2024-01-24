b54b172dc61387a2c74386c8eec795d96cb7c56d
happine2s <happine2s@sookmyung.ac.kr>
2024-01-24 21:36:52 +0900

    [feat] 합격자 선정 취소 기능 구현

---
+++ src/main/java/com/smlikelion/webfounder/manage/exception/InternalServerCandidateException.java
@@ -0,0 +1,5 @@
+package com.smlikelion.webfounder.manage.exception;
+
+public class InternalServerCandidateException extends RuntimeException{
+    public InternalServerCandidateException(String message) { super(message); }
+}
---
+++ src/main/java/com/smlikelion/webfounder/manage/exception/NotFoundCandidateException.java
@@ -0,0 +1,5 @@
+package com.smlikelion.webfounder.manage.exception;
+
+public class NotFoundCandidateException extends RuntimeException{
+    public NotFoundCandidateException(String message) { super(message);}
+}
