ce190fe9fc3e90eb3fa4ad2e0eb476c66b9abbc5
summit45 <brandy2145@gmail.com>
2024-02-16 23:42:46 +0900

    [chore] 사용하지 않는 import 제거

--- src/main/java/com/smlikelion/webfounder/Recruit/Entity/Answer.java
+++ src/main/java/com/smlikelion/webfounder/Recruit/Entity/Answer.java
@@ -3,11 +3,8 @@ import com.smlikelion.webfounder.global.entity.DateEntity;
 import com.smlikelion.webfounder.manage.entity.Question;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "answer")
 @Getter
@@ -18,15 +15,10 @@ public class Answer extends DateEntity {
     @Column(name = "answer_id")
     private Long answerId;
 
-
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;
 
-
     @Column(name = "content", nullable = false)
     private String content;
-
-
 }
